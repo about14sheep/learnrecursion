3e62b85df5f4f25515d65eccb24382b6bebda592
about14sheep <aburger91@gmail.com>
2022-01-23 22:06:30 -0600

    redirects to https

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+if(process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https') {
+            res.redirect(`https://${req.header('host')}${req.url}`);
+        } else {
+            next();
+        }
+    });
+}
+
 const port = process.env.PORT || 8080;
 
 let count = 0;
