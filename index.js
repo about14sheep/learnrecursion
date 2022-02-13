8ea1166b28220d04ba3036148e93f627331741e5
about14sheep <aburger91@gmail.com>
2022-02-13 14:57:02 -0600

    working favicon

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use('/favicon.ico', express.static('views/favicon.ico'));
 
 if(process.env.NODE_ENV === 'production') {
     app.use((req, res, next) => {
